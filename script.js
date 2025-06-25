225e0627769e263a6564c9cf608ec71c19a60ce6
Pedro Coppo Silva <64929940+Pedrada66@users.noreply.github.com>
2025-06-24 22:51:49 -0300

    Limpar e excluir

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Verificar se existem itens no localStorage
+if(localStorage.getItem('cadastros')!=null & localStorage.getItem('cadastros')!=[]){
+    const lista = document.createElement('ul');
+    const formulario = document.getElementById('formulario');
+    formulario.appendChild(lista);
+}
+
+function CriarItem(){
+    
+}
+//verificar se existe intens em localStorage.getItem('cadastros')
+//caso haja, criar ul e com calores de li
+//senão nem cria pfvr
+
 // Limpar campos de entrada
 const entradaNome = document.getElementById("nome");
 const entradaEmail = document.getElementById("email");
@@ -9,24 +23,65 @@ limpar.addEventListener('click', ()=>{
     entradaEmail.value = "";
 });
 
+
+
 // Cadastrar 
 function Cadastrar(){
     let nome = entradaNome.value;
     let email = entradaEmail.value;
-
+    console.log(1);
     const btnCadastrar = document.getElementById('cadastrar');
 
     btnCadastrar.addEventListener('click', ()=>{
         const data = new Date();
         const dia = data.getDate();
-        const mes = data.getMonth();
+        const mes = data.getMonth() + 1; //não sei pq mas tava dando maio como o mês
         const ano = data.getFullYear();
         let datas = `${dia}/${mes}/${ano}`;
-
-        let local = localStorage.getItem('cadastros');
-        let dados = local+`${datas}, ${nome}, ${email}`;
-        dados = JSON.stringify(dados);
-        local = localStorage.setItem('cadastros', dados);
-        console.log(local);
+        //Verifica se usuário entrou com algo nos campos de entrada
+        if(nome!="" & email!=""){
+            let cadastroLocal = localStorage.getItem('cadastros');
+            cadastroLocal = JSON.parse(cadastroLocal) || [];
+            let novoCadastro = {data:datas,nome:nome,email:email};
+            cadastroLocal.push(novoCadastro);
+            dados = JSON.stringify(cadastroLocal);
+            local = localStorage.setItem('cadastros', dados);
+        }
     });
+}
+
+//Limpar
+function Limpar(){
+    const btnLimpar = document.getElementById('limpar');
+
+    btnLimpar.addEventListener('click', ()=>{
+        localStorage.clear();
+    });
+}
+
+//Excluir
+function Excluir(){
+    const btnExcluir = document.getElementById('excluir');
+    let nome = entradaNome.value;
+    let email = entradaEmail.value;
+
+    btnExcluir.addEventListener('click', ()=>{
+        local = localStorage.getItem('cadastros');
+        console.log(local);
+        local = JSON.parse(local);
+        const localExcluido = local.filter(item => item.nome === nome);
+        local = JSON.stringify(localExcluido);
+        console.log(local);
+        localStorage.setItem('cadastros', local);
+    });
+}
+
+//puxar do localStorage, fazer virar uma lista
+//verificar se o que foi colocado com entrada está na lista (iterar com .forEach())
+//guardar ela como excluido
+//excluir com localStorage.removaItem("cadastros", excluido);
+
+//Pesquisar
+function Pesquisar(){
+
 }
